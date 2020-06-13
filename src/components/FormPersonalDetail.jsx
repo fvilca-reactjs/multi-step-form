0edf0230f1ef3371ec091c3ad6e5b2534c4a9685
fvilca1 <fvilca@gmail.com>
2020-06-12 21:03:08 -0500

    step 2: user data

--- src/components/FormPersonalDetail.jsx
+++ src/components/FormPersonalDetail.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const FormPersonalDetail = ({ step,occupation,city, bio, nextStep, prevStep}) => {
+const FormPersonalDetail = ({ step,occupation,city, bio, nextStep, prevStep, handleChange}) => {
     console.log(city)
     //console.log(userData)
     return ( 
         <>
             <h1>Personal Detail</h1>
-            <label>occupation: <input type="text" name={occupation} value={occupation}/> </label><br/>
-            <label>city: <input type="text" name={city} value={city}/></label><br/>
-            <label>bio: <input type="textarea" name={bio} value={bio}/> </label><br></br>
+            <label>occupation: <input type="text" onChange={handleChange} name="occupation" value={occupation}/> </label><br/>
+            <label>city: <input type="text" onChange={handleChange} name="city" value={city}/></label><br/>
+            <label>bio: <input type="text" onChange={handleChange} name="bio" value={bio}/> </label><br></br>
             <button onClick={nextStep}>Continue</button>
             <button onClick={prevStep} >Back</button>
         </>
