242c493119e223d0c6e6027da4a354efbeb16a8d
fvilca1 <fvilca@gmail.com>
2020-06-12 21:02:55 -0500

    step 1: user data

--- src/components/FormUserDetail.jsx
+++ src/components/FormUserDetail.jsx
@@ -1,14 +1,15 @@
 
 import React from 'react'
 
-const FormUserDetail = ({ step, firstName, lastName, email, occupation,city, bio, nextStep, handleChange}) => {
-        
+const FormUserDetail = ({ firstName, lastName, email, occupation,city, bio, nextStep, handleChange}) => {
+    
+
     return ( 
         <>
             <h1>User Detail</h1>
-            <label>firstName: <input type="text" onChange={()=>handleChange} name={firstName} value={firstName}/> </label><br/>
-            <label>LastName: <input type="text" name={lastName} value={lastName}/></label><br/>
-            <label>email: <input type="text" name={email} value={email}/> </label><br></br>
+            <label>firstName: <input type="text" onChange={handleChange} name="firstName" value={firstName} /> </label><br/>
+            <label>LastName: <input type="text" onChange={handleChange} name="lastName" value={lastName}/></label><br/>
+            <label>email: <input type="text" onChange={handleChange} name="email" value={email}/> </label><br></br>
             <button onClick={nextStep}>Continue</button>
         </>
     )
